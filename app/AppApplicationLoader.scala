09d4844ef6477337bba11a8549004b2e57e17509
abelmaneiro <32059405+abelmaneiro@users.noreply.github.com>
2023-11-19 19:45:04 +0000

    Working with Play - Intercepting requests with filters

--- app/AppApplicationLoader.scala
+++ app/AppApplicationLoader.scala
@@ -1,6 +1,6 @@
 import controllers.Application
 import play.api.ApplicationLoader.Context
-import play.api.{ApplicationLoader, BuiltInComponentsFromContext, LoggerConfigurator}
+import play.api.{ApplicationLoader, BuiltInComponentsFromContext, Logger, LoggerConfigurator}
 import play.api.libs.ws.ahc.AhcWSComponents
 import play.api.mvc.DefaultControllerComponents
 import router.Routes
@@ -9,6 +9,8 @@ import com.softwaremill.macwire._
 import _root_.controllers.AssetsComponents
 import play.filters.HttpFiltersComponents
 import services.{SunService, WeatherService}
+
+import scala.concurrent.Future
 
 class AppApplicationLoader extends ApplicationLoader {
   def load(context: Context) = {
@@ -24,6 +26,9 @@ class AppApplicationLoader extends ApplicationLoader {
 // E.g. AhcWSComponents trait gives us an implementation of WSClient
 class AppComponents(context: Context) extends BuiltInComponentsFromContext(context) with AhcWSComponents
   with AssetsComponents with HttpFiltersComponents {
+
+  private val log = Logger(this.getClass)  // logger with this classname
+
   override lazy val controllerComponents = wire[DefaultControllerComponents]
   lazy val prefix: String = "/"
   lazy val router: Router = wire[Routes]
@@ -32,4 +37,14 @@ class AppComponents(context: Context) extends BuiltInComponentsFromContext(conte
   lazy val sunService = wire[SunService]  // Use wire instead of new SunService(wsClient)
   lazy val weatherService = wire[WeatherService]
 
+  // BuiltInComponentsFromContext trait allows adding a stop hook
+  applicationLifecycle.addStopHook { () =>
+//    Future.successful{ log.info("The app is stopping") }  //
+    log.info("The app is about to stop")
+    Future.successful(())  // returns successful future of ()
+  }
+
+  val onStart = {  // Helps readability but don't really need to wrap in a val
+    log.info("The app is about to start")
+  }
 }
